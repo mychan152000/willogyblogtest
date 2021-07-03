d5a4f4d4fdc03fddebb5371017fe40e4872c0f7a
mychan152000 <jmckenzie152000@gmail.com>
2021-07-03 18:37:12 +0700

    update vssue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 // Import main css
 import '~/assets/style/index.scss'
 import Vue from 'vue'
-import Vssue from 'vssue';
-import GithubV3 from '@vssue/api-github-v3';
-import 'vssue/dist/vssue.css'
-
-
 // Import default layout so we don't need to import it to every page
 import DefaultLayout from '~/layouts/Default.vue'
 import BootstrapVue from 'bootstrap-vue'
@@ -23,11 +18,4 @@ export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
   Vue.use(BootstrapVue)
-  Vue.use(Vssue, {
-    api: GithubV3,
-    owner: 'mychan152000',
-    repo: 'willogyblogtest',
-    clientId: '8df817f6fe85b570f293',
-    clientSecret: '136ef75400102361a3e55bd32a8e3dc1004283a3',
-  })
 }
