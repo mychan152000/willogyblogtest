002544cf82cdd54bee6ab33bb61488028ad3d5fa
mychan152000 <jmckenzie152000@gmail.com>
2021-07-16 01:09:58 +0700

    add sitemap and robot.txt

--- gridsome.config.js
+++ gridsome.config.js
@@ -29,7 +29,41 @@ module.exports = {
           }
         }
       }
-    }
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        config: {
+          '/': {
+            changefreq: 'weekly',
+            priority: 0.5,
+          },
+          
+        }
+      }
+    },
+    {
+      use: 'gridsome-plugin-robots-txt',
+      options: {
+        host: 'https://insights.willogy.io/',
+        sitemap: 'https://insights.willogy.io//configs/sitemap.xml',
+        policy: [
+          {
+            userAgent: "Googlebot",
+            allow: "/",
+            disallow: "/search",
+            crawlDelay: 2
+          },
+          {
+            userAgent: "*",
+            allow: "/",
+            disallow: "/search",
+            crawlDelay: 10,
+            cleanParam: "ref /"
+          }
+        ]
+      }
+    },
   ],
 
   transformers: {
