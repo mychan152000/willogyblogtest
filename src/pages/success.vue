26853c1edb15a5bbbcdda151713b6f6385a099d6
mychan152000 <jmckenzie152000@gmail.com>
2021-08-07 02:56:49 +0700

    edit success page

---
+++ src/pages/success.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="container">
+    <Layout :show-logo="true">
+        Form sent successfully
+    </Layout>
+</div>
+  
+</template>
+
+
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+
+
+</style>
