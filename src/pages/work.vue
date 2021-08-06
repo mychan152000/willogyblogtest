7feae908eff711e45b8f5bfbe8236d30851525ee
mychan152000 <jmckenzie152000@gmail.com>
2021-08-07 03:00:04 +0700

    rework work page

--- src/pages/work.vue
+++ src/pages/work.vue
@@ -118,7 +118,7 @@ export default {
                 ...this.formData,
             }),
             })
-            .then(() => alert("Success"))
+            .then(() => this.$router.push('/success'))
             .catch(error => alert(error))
         }
         }
