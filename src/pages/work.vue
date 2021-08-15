432c91eb5a880f7f14618c2b652aefe9cf301f0b
mytran <jmckenzie152000@gmail.com>
2021-08-15 20:43:21 +0700

    test edit in function, refresh page

--- src/pages/work.vue
+++ src/pages/work.vue
@@ -1,143 +1,158 @@
 <template>
-<div class="container">
-    <Layout :show-logo="true">
-        <div class="row title">
-            <div class="mx-auto">Work With Us</div>
-        </div>
-        <div class="row">
-            <g-image src="~/assets/handshake.png" style="width:100%"/>
-        </div>
-        <form
-            name="contact"
-            method="post"
-            v-on:submit.prevent="handleSubmit"
-            action="/success/"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-            >
-            <input type="hidden" name="form-name" value="contact" />
-            <p hidden>
-                <label>
-                Don’t fill this out: <input name="bot-field" />
-                </label>
-            </p>
-            <div class="sender-info">
-                <div>
-                <label for="name" class="label" >Your name</label>
-                <input type="text" name="name" v-model="formData.name" />
-                </div>
-                <div>
-                <label for="email">Your email</label>
-                <input type="email" name="email" v-model="formData.email" />
-                </div>
-            </div>
 
-            <div class="message-wrapper">
-                <label for="message">Message</label>
-                <textarea name="message" v-model="formData.message"></textarea>
-            </div>
+  <div id="app">
+    <div class="container">
+      <header class="header">
+        <div class="header__left">
+          <Logo v-if="showLogo" />
+        </div>
 
-            <button type="submit">Submit form</button>
-        </form>
-        <div class="row head">
-            <div class="mx-auto">Willogy's history</div>
+        <div class="header__right">
+          <g-link to="/about" style="padding-right: 2em; color: black">About us</g-link>
+          <g-link to="/work" style="color: black">Work with us</g-link>
         </div>
-        <div class="row">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dui metus, malesuada a tincidunt non, facilisis sit amet sem. Aliquam pharetra vitae est a mollis. Integer congue auctor quam. Sed felis ligula, vulputate ac euismod a, ullamcorper luctus dolor. In hac habitasse platea dictumst. Aenean eleifend luctus mi. Phasellus condimentum lorem enim, at consequat libero commodo ut.</p>
+      </header>
+
+      <main class="main">
+        <slot/>
+      </main>
+
+
+      <section class="newsletter">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12">
+              <div class="content">
+                <h2>Join Our Newsletter</h2>
+                <h3></h3>
+                <div class="input-group">
+                      <input type="email" class="form-control" placeholder="Enter your email">
+                        <span class="input-group-btn">
+                          <button class="btn" type="submit">Subscribe Now</button>
+                        </span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="row head">
-            <div class="mx-auto">Our Goals</div>
-        </div>
-        <div class="row">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dui metus, malesuada a tincidunt non, facilisis sit amet sem. Aliquam pharetra vitae est a mollis. Integer congue auctor quam. Sed felis ligula, vulputate ac euismod a, ullamcorper luctus dolor. In hac habitasse platea dictumst. Aenean eleifend luctus mi. Phasellus condimentum lorem enim, at consequat libero commodo ut.
-            </p>
-        </div>
-        <div class="row head">
-            <div class="mx-auto">Take a look at the Office</div>
-        </div>
-        <div class="row">
-            <g-image src="~/assets/handshake.png" style="width:100%"/>
-        </div>
-    </Layout>
-</div>
-  
+      </section>
+      <footer class="footer flex">
+        <span class="footer__copyright">Copyright © Willogy {{ new Date().getFullYear() }}. </span>
+        <ul class="footer__list">
+          <li>Follow on <a href="https://willogy.io" style="color:#6B17E6" target="_blank">Willogy.io</a></li>
+          <li><a href="https://willogy.io/#" target="_blank">Products</a></li>
+          <li><a href="https://willogy.io/#about" target="_blank">About</a></li>
+        </ul>
+      </footer>
+    </div>
+  </div>
 </template>
 
-
 <script>
+
+import Logo from '~/components/Logo.vue'
+// import ToggleTheme from '~/components/ToggleTheme.vue'
+
 export default {
-    data() {
-        return {
-            formData: {},
-        }
-    },
-    metaInfo() {
-        return {
-        link: [
-        {rel: 'canonical', href: 'https://insights.willogy.io/work'}
-        ],
-        title: 'Willogy Insights | Work',
-        meta: [
-            {
-            property: "og:title",
-            content: 'Willogy Insights'
-            },
-            {
-            name: "twitter:card",
-            content: './src/assets/logo.svg' ? "summary_large_image" : "summary",
-            },
-            
-            {
-            property: "og:description",
-            content: 'Knowledge is common. Our insights and experience on-top of them is unique'
-            },
-            {
-            property: "og:type",
-            content: 'article, blog, content, research, insights'
-            },
-            
-            {
-            property: "og:image",
-            content: './src/assets/logo.svg' || ""
-            },
-        ]
-        };
-    },
-    methods: {
-        encode(data) {
-            return Object.keys(data)
-            .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-            .join('&')
-        },
-        handleSubmit(e) {
-            fetch('/', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: this.encode({
-                'form-name': e.target.getAttribute('name'),
-                ...this.formData,
-            }),
-            })
-            .then(() => this.$router.push('/success'))
-            .catch(error => alert(error))
-        }
-        }
+  props: {
+    showLogo: { default: true }
+  },
+  components: {
+    Logo,
+  }
 }
 </script>
-<style scoped>
-* {
-  font-family: Helvetica, sans-serif;
+
+<style lang="scss">
+.footer li{
+  display: inline;
+   margin: 0px 12px;   
+
+
 }
-.title {
-  font-size: calc(15px + 2.5vw);
-  font-weight: 700;
-}
-.head {
-  font-weight: 700;
-  font-size: calc(15px + 1vw);
-}
-.row {
-  padding: 0.6em 0em;
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: var(--header-height);
+  padding: 0 calc(var(--space) / 2);
+  top:0;
+  z-index: 10;
+  background-color: #ffffff;
+  border-bottom: 1px solid #ececec;
+
+  &__left,
+  &__right {
+    display: flex;
+    align-items: center;
+  }
+
+  @media screen and (min-width: 1300px) {
+    //Make header sticky for large screens
+    position: sticky;
+    width: 100%;
+  }
 }
 
+.main {
+  margin: 0 auto;
+  padding: 1.5vw 15px 0;
+}
+
+.footer {
+  display: flex;
+  justify-content: space-between;
+  padding: calc(var(--space) / 2);
+  text-align: center;
+  font-size: .8em;
+
+  > span {
+    margin: 0 .35em;
+    
+  }
+
+  a {
+    color: currentColor;
+  }
+  &__list {
+    list-style-type: none;
+  }
+}
+
+a {
+  color: black;
+  text-decoration: none !important;
+}
+
+.newsletter {
+padding: 30px 0px;
+background: white;
+box-shadow: 0px 2px 8px 0px;
+}
+
+.newsletter .content {
+max-width: 500px;
+margin: 0 auto;
+text-align: center;
+position: relative;
+z-index: 2; }
+.newsletter .content h2 {
+color: #243c4f;
+margin-bottom: 40px; }
+.newsletter .content .form-control {
+height: 50px;
+border-color: gray;
+border-radius:0;
+}
+.newsletter .content.form-control:focus {
+box-shadow: none;
+border: 2px solid #243c4f;
+}
+.newsletter .content .btn {
+min-height: 50px; 
+border-radius:0;
+background: #243c4f;
+color: #fff;
+font-weight:600;
+}
 </style>
